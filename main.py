39757ba0b8ef85320b4a778fb6fa14ebfaa083c8
OnlyMagic <ivansmirnov9719@gmail.com>
2026-04-06 09:18:56 -0400

    3

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ import zipfile
 import xml.etree.ElementTree as ET
 
 # Чтение из файла
-tree = ET.parse('/home/kali/Desktop/BDU/export')
+tree = ET.parse('/home/kali/Desktop/BDU/export/vulxml.xml')
 root = tree.getroot()
 
 
 
-# Поиск всех уязвимостей
 for vul in root.findall('.//vul'):
-    # Получаем cve_id (с проверкой, что тег существует)
-    cve_element = vul.find('cve_id')
-    cve_id = cve_element.text if cve_element is not None else "Нет CVE"
-    
-    # Получаем тип идентификатора
-    identifiers_element = vul.find('identifiers')
-    if identifiers_element is not None:
-        identifier_element = identifiers_element.find('identifier')
-        if identifier_element is not None:
-            ident_type = identifier_element.get('type')
-        else:
-            ident_type = "Нет типа"
+    identifier = vul.find('.//identifier')
+    if identifier is not None:
+        cve_id = identifier.text
+        ident_type = identifier.get('type')
     else:
-        ident_type = "Нет идентификаторов"
+        cve_id = "Нет CVE"
+        ident_type = "Нет типа"
     
     print(f"CVE ID: {cve_id}, Тип: {ident_type}")
+
+for vul in root.findall('.//vul'):
+    name_element = vul.find('name')
+    if name_element is not None:
+        name = name_element.text
+    else:
+        name = "Нет названия"
+    
+    print(f"Имя уязвимости: {name}")
 
 
 
