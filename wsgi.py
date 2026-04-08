6371181e67a5ee5baee333e7020677be55557af4
OnlyMagic <ivansmirnov9719@gmail.com>
2026-04-08 08:04:13 -0400

    1

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ import config
 from flask import Flask, render_template
 from flask import request
 from flask import Flask, render_template, request, redirect
+from urllib.parse import urlencode
 
 app = Flask(__name__)
 
@@ -9,11 +10,20 @@ app = Flask(__name__)
 
 @app.route('/cve/<identifier>')
 def cve_detail(identifier):
-    back_url = request.args.get('back', '/full_cve')
+    # Получаем параметры для возврата
+    back_page = request.args.get('page', 1, type=int)
+    back_limit = request.args.get('limit', 20, type=int)
+    back_query = request.args.get('q', '')
+    
+    if back_query:
+        back_url = f"/search?q={back_query}&page={back_page}&limit={back_limit}"
+    else:
+        back_url = f"/full_cve?page={back_page}&limit={back_limit}"
+    
     vuln = config.get_vuln_by_identifier(identifier)
     if vuln is None:
         return "Уязвимость не найдена", 404
-    return render_template('cve.html', vuln=vuln, back_url=back_url)
+    return render_template('cve.html', vuln=vuln, back_url=back_url, back_query=back_query, back_page=back_page, back_limit=back_limit)
 
 
 @app.route('/full_cve')
